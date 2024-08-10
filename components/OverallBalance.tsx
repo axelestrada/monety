fbb401f94db388db9078355dd6c52e8eb4b04f97
Axel Estrada <axele1524@gmail.com>
2024-08-09 18:01:02 -0600

    add overall balance component

---
+++ components/OverallBalance.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+const OverallBalance = () => {
+  return (
+    <View className="justify-center items-center py-5">
+      <Text className="text-main-500">Overall balance</Text>
+      <Text className="text-main font-[Rounded-Bold] text-4.5xl">L 4,125.83</Text>
+    </View>
+  );
+};
+
+export default OverallBalance;
