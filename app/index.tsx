78a9257d91389e75f4c930842124297cde2f9100
Axel Estrada <axele1524@gmail.com>
2024-10-05 12:06:00 -0600

    Add Statistics by day support

--- app/index.tsx
+++ app/index.tsx
@@ -24,7 +24,7 @@ import IconButton from "@/components/ui/IconButton";
 import { Octicons } from "@expo/vector-icons";
 import { useSQLiteContext } from "expo-sqlite";
 
-import { LineChart } from "react-native-gifted-charts";
+import { LineChart, lineDataItem } from "react-native-gifted-charts";
 
 import uuid from "react-native-uuid";
 import { defaultCategories } from "@/constants/categories";
@@ -32,6 +32,8 @@ import { useAccounts, useCategories, useTransactions } from "@/hooks";
 import { useAppDispatch, useTypedSelector } from "@/store";
 import { ITransaction } from "@/interfaces";
 import { transactionServices } from "@/reducers/transactionsSlice";
+import TimeRange from "@/components/TimeRange";
+import moment from "moment";
 
 export default function Index() {
   const [analyticsType, setAnalyticsType] = useState<"Incomes" | "Expenses">(
@@ -50,29 +52,13 @@ export default function Index() {
 
   const [refreshing, setRefreshing] = useState(false);
 
+  const [incomes, setIncomes] = useState<lineDataItem[]>([{ value: 0 }]);
+  const [expenses, setExpenses] = useState<lineDataItem[]>([{ value: 0 }]);
+
+  const [maxValue, setMaxValue] = useState(100);
+  const [breakpoints, setBreakpoints] = useState<number[]>([]);
+
   const { colorScheme, toggleColorScheme } = useColorScheme();
-
-  const lineData = [
-    { value: 0, dataPointText: "0" },
-    { value: 10, dataPointText: "10" },
-    { value: 8, dataPointText: "8" },
-    { value: 58, dataPointText: "58" },
-    { value: 56, dataPointText: "56" },
-    { value: 78, dataPointText: "78" },
-    { value: 74, dataPointText: "74" },
-    { value: 98, dataPointText: "98" },
-  ];
-
-  const lineData2 = [
-    { value: 15, dataPointText: "15" },
-    { value: 20, dataPointText: "20" },
-    { value: 18, dataPointText: "18" },
-    { value: 40, dataPointText: "40" },
-    { value: 36, dataPointText: "36" },
-    { value: 60, dataPointText: "60" },
-    { value: 54, dataPointText: "54" },
-    { value: 85, dataPointText: "85" },
-  ];
 
   const onRefresh = useCallback(() => {
     setRefreshing(true);
@@ -206,6 +192,207 @@ export default function Index() {
     }
   }, []);
 
+  useEffect(() => {
+    const reversedTransactions = [...transactions]
+      .reverse()
+      .filter((transaction) => transaction.type !== "Transfer");
+
+    if (reversedTransactions.length > 0) {
+      const newIncomes: lineDataItem[] = [];
+      const newExpenses: lineDataItem[] = [];
+
+      let newMaxValue = 0;
+
+      const minDate = moment(
+        Math.min(
+          ...reversedTransactions.map((transaction) => transaction.date * 1000)
+        )
+      )
+        .startOf("hour")
+        .unix();
+
+      const maxDate = moment(
+        Math.max(
+          ...reversedTransactions.map((transaction) => transaction.date * 1000)
+        )
+      )
+        .add(1, "hour")
+        .startOf("hour")
+        .unix();
+
+      const hoursOfDifference = moment(maxDate * 1000).diff(
+        minDate * 1000,
+        "hours"
+      );
+
+      console.log(hoursOfDifference);
+
+      let newBreakPoints: number[] = [0, 0, 0, 0, 0];
+
+      if (hoursOfDifference <= 4) {
+        newBreakPoints = newBreakPoints.map((val, idx) => {
+          switch (idx) {
+            case 0:
+              return minDate;
+            case 1:
+              return moment(minDate * 1000)
+                .add(1, "hour")
+                .unix();
+            case 2:
+              return moment(minDate * 1000)
+                .add(2, "hour")
+                .unix();
+            case 3:
+              return moment(minDate * 1000)
+                .add(3, "hour")
+                .unix();
+            case 4:
+              return moment(minDate * 1000)
+                .add(4, "hour")
+                .unix();
+            default:
+              return 0;
+          }
+        });
+      } else {
+        const interval = Math.ceil(hoursOfDifference / 4);
+
+        newBreakPoints = newBreakPoints.map((val, idx) => {
+          switch (idx) {
+            case 0:
+              return minDate;
+            case 1:
+              return moment(minDate * 1000)
+                .add(1 * interval, "hour")
+                .unix();
+            case 2:
+              return moment(minDate * 1000)
+                .add(2 * interval, "hour")
+                .unix();
+            case 3:
+              return moment(minDate * 1000)
+                .add(3 * interval, "hour")
+                .unix();
+            case 4:
+              return moment(minDate * 1000)
+                .add(4 * interval, "hour")
+                .unix();
+            default:
+              return 0;
+          }
+        });
+      }
+
+      setBreakpoints(newBreakPoints);
+
+      newBreakPoints.forEach((value, idx) => {
+        const rangeTransactions = transactions.filter(
+          (transaction) =>
+            transaction.date >= value &&
+            transaction.date <= newBreakPoints[idx + 1]
+        );
+
+        const incomesAmount = Math.round(
+          rangeTransactions
+            .filter((transaction) => transaction.type === "Income")
+            .reduce((acc, curr) => acc + curr.amount, 0)
+        );
+
+        const expensesAmount = Math.round(
+          rangeTransactions
+            .filter((transaction) => transaction.type === "Expense")
+            .reduce((acc, curr) => acc + curr.amount, 0)
+        );
+
+        const incomesLabelSize = (incomesAmount.toString().length + 1) * 14;
+
+        newMaxValue = incomesAmount > newMaxValue ? incomesAmount : newMaxValue;
+
+        newIncomes.push({
+          value: incomesAmount,
+          focusedDataPointLabelComponent: () => (
+            <View
+              className="dark:bg-[#5bbe77] bg-green rounded py-0.5 items-center"
+              style={{
+                width: incomesLabelSize,
+                top:
+                  incomesAmount === expensesAmount
+                    ? 5
+                    : incomesAmount > expensesAmount
+                    ? 5
+                    : 50,
+                left: 22,
+                transform: [{ translateX: -(incomesLabelSize / 2) }],
+              }}
+            >
+              <Text className="font-[Rounded-Medium] text-white dark:text-[#F5F5F5]">
+                L{incomesAmount}
+              </Text>
+            </View>
+          ),
+          focusedCustomDataPoint: () => (
+            <View className="bg-green dark:bg-[#5bbe77] justify-center items-center w-[14] h-[14] -top-[4] rounded-full">
+              <View className="bg-white dark:bg-[#F5F5F5] w-[8] h-[8] rounded-full"></View>
+            </View>
+          ),
+        });
+
+        const expensesLabelSize = (expensesAmount.toString().length + 1) * 14;
+
+        newMaxValue =
+          expensesAmount > newMaxValue ? expensesAmount : newMaxValue;
+
+        newExpenses.push({
+          value: expensesAmount,
+          focusedDataPointLabelComponent: () => (
+            <View
+              className="bg-[#ff8092] py-0.5 top-1 rounded items-center"
+              style={{
+                width: expensesLabelSize,
+                top:
+                  expensesAmount === incomesAmount
+                    ? 50
+                    : expensesAmount > incomesAmount
+                    ? 5
+                    : 50,
+                left: 22,
+                transform: [{ translateX: -(expensesLabelSize / 2) }],
+              }}
+            >
+              <Text className="font-[Rounded-Medium] text-white dark:text-[#F5F5F5]">
+                L{expensesAmount}
+              </Text>
+            </View>
+          ),
+          focusedCustomDataPoint: () => (
+            <View className="bg-[#ff8092] justify-center items-center w-[14] h-[14] -top-[4] rounded-full">
+              <View className="dark:bg-[#F5F5F5] bg-white w-[8] h-[8] rounded-full"></View>
+            </View>
+          ),
+        });
+      });
+
+      setMaxValue(newMaxValue);
+
+      setIncomes([
+        { value: newIncomes[0].value },
+        ...newIncomes,
+        { value: newIncomes[newIncomes.length - 1].value },
+      ]);
+
+      setExpenses([
+        { value: newExpenses[0].value },
+        ...newExpenses,
+        { value: newExpenses[newExpenses.length - 1].value },
+      ]);
+    } else {
+      setIncomes([{ value: 0 }, { value: 0 }]);
+      setExpenses([{ value: 0 }, { value: 0 }]);
+      setMaxValue(100);
+      setBreakpoints([]);
+    }
+  }, [transactions]);
+
   // #region Load Fonts
   const [fontsLoaded, fontError] = useFonts({
     "Rounded-Regular": require("../assets/fonts/Rounded-Regular.ttf"),
@@ -245,7 +432,9 @@ export default function Index() {
         </IconButton>
       </Header>
 
-      <OverallBalance />
+      <OverallBalance>
+        <TimeRange />
+      </OverallBalance>
 
       <ScrollView
         className="mt-2 -mb-6"
@@ -261,7 +450,7 @@ export default function Index() {
           />
         }
       >
-        <View className="bg-white dark:bg-[#1A1A1A] rounded-2xl pt-2 mx-3 overflow-hidden">
+        <View className="bg-white dark:bg-[#1A1A1A] rounded-2xl py-2 mx-3 overflow-hidden">
           <View className="mx-2 flex-row justify-between">
             <Text className="text-main dark:text-[#F5F5F5] text-lg font-[Rounded-Bold]">
               Statistics
@@ -284,32 +473,56 @@ export default function Index() {
             </View>
           </View>
 
-          <View className="-mx-[10]">
+          <View className="-mx-[10] mt-4">
             <LineChart
+              height={180}
+              maxValue={maxValue + 32}
+              overflowTop={100}
+              overflowBottom={100}
               isAnimated
               curved
               color1={colorScheme === "dark" ? "#5bbe77" : "#02AB5B"}
               color2={colorScheme === "dark" ? "#FF8092" : "#FF8092"}
-              data={lineData}
-              data2={lineData2}
+              data={incomes}
+              data2={expenses}
               initialSpacing={0}
+              dataPointLabelWidth={0}
               endSpacing={0}
-              hideDataPoints
               thickness={5}
               curvature={0.2}
+              focusEnabled
+              delayBeforeUnFocus={2000}
+              customDataPoint={() => <></>}
               hideRules
+              noOfSections={4}
+              stepValue={maxValue / 4}
               hideYAxisText
-              hideOrigin
               yAxisThickness={0}
               xAxisThickness={0}
+              xAxisLabelTextStyle={{
+                color: colorScheme === "dark" ? "#F5F5F580" : "#1B1D1C80",
+                fontFamily: "Rounded-Regular",
+                fontSize: 12,
+              }}
+              xAxisLabelsVerticalShift={20}
               adjustToWidth
               disableScroll
               width={windowWidth - 21}
             />
           </View>
-        </View>
 
-        {/* <AnalyticsChart type={analyticsType} /> */}
+          <View className="mx-[40] flex-row justify-between items-center">
+            {breakpoints &&
+              breakpoints.map((date, idx) => (
+                <Text
+                  key={date + idx}
+                  className="text-main-500 dark:text-[#F5F5F580] font-[Rounded-Regular] text-xs"
+                >
+                  {moment(date * 1000).format("hh:mm A")}
+                </Text>
+              ))}
+          </View>
+        </View>
 
         <View className="flex flex-row mt-3 mx-1.5">
           <CashFlowItem
