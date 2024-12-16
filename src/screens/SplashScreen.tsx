010e856a824d5924314d5e6fc1ea949e3d3aea22
Axel Estrada <axele1524@gmail.com>
2024-12-16 16:03:24 -0600

    refactor: replace LoadingScreen with SplashScreen for improved user experience

---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,34 @@
+import { ActivityIndicator, Image, View } from "react-native";
+
+import Screen from "@/components/Screen";
+import CustomText from "@/components/CustomText";
+
+import useThemeColors from "@/hooks/useThemeColors";
+
+export const SplashScreen = () => {
+  const colors = useThemeColors();
+
+  return (
+    <Screen>
+      <View className="flex-[1] justify-center items-center">
+        <Image
+          source={require("@/assets/images/icon.png")}
+          style={{
+            width: 64,
+            height: 64,
+            resizeMode: "contain",
+            borderRadius: 16,
+          }}
+        />
+
+        <CustomText className="text-text-primary text-3xl font-[Rounded-Bold] mt-2">Monety</CustomText>
+      </View>
+
+      <ActivityIndicator
+        size={"large"}
+        className="mb-4"
+        color={colors["--color-accent"]}
+      />
+    </Screen>
+  );
+};
