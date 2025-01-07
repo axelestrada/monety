eebd42c3177e633b9651cd635c72d1628bd301c5
Axel Estrada <axele1524@gmail.com>
2025-01-06 22:27:11 -0600

    fix: adjust width of RowsOutline icon for better alignment

--- src/assets/icons/RowsOutline.tsx
+++ src/assets/icons/RowsOutline.tsx
@@ -3,7 +3,7 @@ import Svg, { Rect, SvgProps } from "react-native-svg";
 
 export const RowsOutline = (props: SvgProps) => {
   return (
-    <Svg width={20} height={20} viewBox="0 0 20 20" fill={props.fill}>
+    <Svg width={18} height={20} viewBox="0 0 20 20" fill={props.fill}>
       <Rect
         width={19}
         height={8}
