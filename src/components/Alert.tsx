5865c4422278606d6bf60faa97c30c510d595940
Axel Estrada <axele1524@gmail.com>
2025-02-08 22:50:08 -0600

    feat: enhance transaction interaction and delete flow

---
+++ src/components/Alert.tsx
@@ -0,0 +1,53 @@
+import { CustomModal } from "@/components/CustomModal";
+import { Text, TouchableOpacity, View } from "react-native";
+import { CustomText } from "@/components/CustomText";
+
+interface AlertProps {
+  isVisible: boolean;
+  title: string;
+  onRequestClose: () => void;
+  onCancel?: () => void;
+  onSuccess?: () => void;
+}
+
+export const Alert = ({
+  title,
+  onCancel = () => {},
+  onSuccess = () => {},
+  ...props
+}: AlertProps) => {
+  return (
+    <CustomModal {...props}>
+      <View
+        className="bg-card-background rounded-3xl"
+        style={{
+          minWidth: 220,
+        }}
+      >
+        <CustomText className="font-[Rounded-Medium] text-text-secondary text-center p-5 text-xs">
+          {title}
+        </CustomText>
+
+        <View className="flex-row border-t border-separator">
+          <TouchableOpacity
+            className="flex-[1] items-center p-4"
+            onPress={onCancel}
+          >
+            <CustomText className="font-[Rounded-Medium] text-xs text-text-primary">
+              CANCEL
+            </CustomText>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            className="flex-[1] items-center p-4"
+            onPress={onSuccess}
+          >
+            <CustomText className="font-[Rounded-Medium] text-xs text-accent">
+              YES
+            </CustomText>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </CustomModal>
+  );
+};
